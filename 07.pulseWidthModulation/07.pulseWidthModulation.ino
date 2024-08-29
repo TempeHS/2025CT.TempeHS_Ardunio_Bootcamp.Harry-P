21639a4c425ed360da07c4cb46c4d9b3606d98d5
PDAHarry10 <Harry.Pham969@education.nsw.gov.au>
2024-08-29 10:35:02 +1000

    Started Lesson 7

--- 07.pulseWidthModulation/07.pulseWidthModulation.ino
+++ 07.pulseWidthModulation/07.pulseWidthModulation.ino
@@ -1,5 +1,5 @@
 /*
-  Author: 
+  Author: Harry P
 
   Learning Intention:
   The students will learn what 'pulse width modulation' is and how to use it to write
@@ -23,10 +23,14 @@
 
 
 
-void setup() {
-  
+void setup() 
+{
+  Serial.begin(9600);
+  Serial.println("Debug");
+  pinMode(3, OUTPUT);
 }
 
-void loop() {
-  
+void loop() 
+{
+  analogWrite(3, analogRead(A3) / 4);
 }
